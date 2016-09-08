ad9ded6413a8176b2f8496cc915fdc41f965bc16
man-ito-ba <pierre.m.global@gmail.com>
2016-09-08 11:54:48 +0200

    Fonction de réception
    La fonction de récéption fonctionne bel et bien, mais il faut pour cela
    procéder à un montage physique !

--- GSM_Squeletton.ino
+++ GSM_Squeletton.ino
@@ -1,5 +1,5 @@
 // ****************************************************************************
-// *                                    GSM                                   *
+// *									GSM								   *
 // ****************************************************************************
 
 // Initialisation de la librairie GSM
@@ -10,19 +10,22 @@ GSM_SMS sms;
 // Code PIN à indiquer rentre les guillemets si nécessaire
 #define CodePIN ""
 // Tableau destiné à conserver le numéro de l'expéditeur d'un SMS reçu par le shield
-char Num_ExpediteurSMS[20];
+char Num_Expediteur[20];
+
+bool SMS_LED;			// variable qui permet l'allumage extinction de la led par texto
+bool SMS_LEDprecedent;	// variable est destiné à l'état précédent de la LED
 
 
 // ****************************************************************************
-// *                                 Matériel                                 *
+// *								 Matériel								 *
 // ****************************************************************************
 
 // ======== LED & variables nécessaires à son fonctionnement
-const int LED = 13;
+const int LED = 12;
 bool LED_Etat;
 
 // ======== Bouton & variables associées à son bon fonctionnement
-const int Bouton = 3;				// Bouton branché au pin 3
+const int Bouton = 4;				// Bouton branché au pin 3
 bool Bouton_Etat;					// Booléenne enregistrant l'état du bouton par la fonction digitalRead()
 bool Bouton_EtatPrecedent = HIGH;	// Booléenne enregistrant l'état précédant du bouton
 									// En l'occurence il est "HIGH" car, branché en INPUT_PULLUP, c'est ce qu'il renvoi en digitalRead
@@ -47,16 +50,15 @@ void setup()
 	}
 
 	// Bool d'état de la connection
-	bool notConnected = true;
+	bool PasDeConnection = true;
 
 	// Démarrage du shield GSM
 	// Si la SIM a un code PIN, le passer en tant que paramètre de begin(), entre guillemets
-	while (notConnected) {
+	while (PasDeConnection) {
 		Serial.println("-- Recherche du reseau...");
 		if (gsmAccess.begin(CodePIN) == GSM_READY) {
-			notConnected = false;
-		} 
-		else {
+			PasDeConnection = false;
+		} else {
 			Serial.println("-- Pas de connection");
 			delay(1000);
 		}
@@ -66,6 +68,8 @@ void setup()
 	Serial.println("**************************");
 	Serial.println("** Programme SMS in/out **");
 	Serial.println("**************************");
+
+	delay(1000);
 
 	Reception();										// Avant de commencer le programme, au depart, on vériie s'il y a déjà des messages en attente
 
@@ -77,10 +81,20 @@ void loop()
 {
 	Reception();
 	EtatDuBouton();
-	AllumageDeLaLED();
+	LEDallumageBouton();
 
 	// Délai nécessaire pour que le programme tourne sans souci
 	delay(10);
+}
+
+void Recapitulatif_Instructions(){
+	// Une fonction appelée par la réception de la suite "1234567890" ou "Instructions" 
+	char InstructionsUtilisateur[] = 
+		"Instructions";
+	// Envoi effectif des instructions
+	sms.beginSMS(Num_Expediteur);
+	sms.print(InstructionsUtilisateur);
+	sms.endSMS();
 }
 
 void Reception(){
@@ -88,12 +102,14 @@ void Reception(){
 
 	// Si un SMS est reçu()
 	if (sms.available()) {
-		Seria.println("Message en cours de reception...");
+		Serial.println("Message en cours de reception...");
 		delay(1000);
-		Serial.println("Expediteur du SMS :");
-		// Obtention et affichage du numéro de l'expéditeur du SMS
-		sms.remoteNumber(Num_ExpediteurSMS, 20);
-		Serial.println(Num_ExpediteurSMS);
+		// Numéro expéditeur (Ø nécessaire ici)
+			// Serial.println("Expediteur du SMS :");
+			// Obtention et affichage du numéro de l'expéditeur du SMS
+			// sms.remoteNumber(Num_Expediteur, 20);
+			// Serial.println(Num_Expediteur);
+	
 		/* Messages qui seront automatiquement supprimés */
 			// Un exemple de supression de message
 			// Ex. : tout message commençant par un "#" devrait être effacé
@@ -102,36 +118,47 @@ void Reception(){
 				sms.flush();
 			}
 		// Lecture des bytes du message et affichage sur le moniteur série
-		while (Texto_LPL = sms.read()) {		// Jusqu'à ce que les bytes lu par la fonction read() correspondent à la variable "c"
+		while (Texto_LPL = sms.read()) {		// Jusqu'à ce que les bytes lu par la fonction read() correspondent à la variable "Texto_LPL"
 			Serial.print(Texto_LPL);
 		}
+	
 		Serial.println("\nFin du message");
+		delay(1000);
+
 		// Suppression des messages de la memoire du modem
 		sms.flush();
 		Serial.println("Suppression du message OK");
-		delay(1000);
 	}
+
 	// S'il n'y a aucun SMS
 	if(!sms.available()){
-	    Serial.println("Pas de nouveau message");
+		Serial.println("Pas de nouveau message");
 	}
 	delay(1000);
-	Instructions();
 }
 
 void EtatDuBouton(){
-	Bouton_Etat = digitalRead(Bouton);
+	Bouton_Etat = digitalRead(Bouton);	// Cette fonction permet à un bouton d'allumer la LED
 }
 
-void AllumageDeLaLED(){
+void LEDallumageBouton(){
+	// Si on appuie sur le bouton, et que son état précédent était "pas appuyé", et qu'on aisse suffisamment de temps
 	if(Bouton_Etat == LOW && Bouton_EtatPrecedent == HIGH && millis() - Bouton_Temps > Bouton_Debounce){
-		if(LED_Etat == HIGH){
-			LED_Etat = LOW;
+		if(LED_Etat == HIGH){			// et que la LED est déjà allumée
+			LED_Etat = LOW;				// on passe l'état (et non pas a led) à "éteint"
 		}
 		else {
-			LED_Etat = HIGH;
+			LED_Etat = HIGH;			// et vice-versa
 		}
 	}
-	digitalWrite(LED, LED_Etat);
-	Bouton_EtatPrecedent = Bouton_Etat;
+	digitalWrite(LED, LED_Etat);		// et on attribue à la LED a valeur de État_led
+	Bouton_EtatPrecedent = Bouton_Etat;	// sans oublier d'enregistrer l'état du bouton
+}
+
+void LEDallumageSMS(){
+
+}
+
+void LedAllumee(){
+	// Cette fonction informe l'utilisateur que la Led a été allumée, que ce soit à distance ou par bouton
 }
